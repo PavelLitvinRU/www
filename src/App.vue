5a2cc59d27f4b34df52212bee234ac1e7cda799c
PavelLitvinRU <litvin.pv@gmail.com>
2018-11-02 23:24:50 +0300

    v0.0.2

--- src/App.vue
+++ src/App.vue
@@ -1,109 +1,95 @@
 <template>
-  <v-app light>
-    <v-navigation-drawer
-      fixed
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      app
-    >
-      <v-list>
-        <v-list-tile 
-          v-for="(item, i) in items"
-          :key="i"
-          value="true"
+<v-dark>
+ <v-parallax
+    dark
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    height="1000"
+  >
+   <v-container style="max-width: 600px;">
+    <v-timeline dense clipped>
+      <v-timeline-item
+        fill-dot
+        class="white--text mb-5"
+        color="orange"
+        large
+      >
+        <span slot="icon">JL</span>
+        <v-text-field
+          v-model="input"
+          hide-details
+          flat
+          label="Leave a comment..."
+          solo
+          @keydown.enter="comment"
         >
-          <v-list-tile-action>
-            <v-icon light v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar app>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn 
-        icon
-        @click.stop="miniVariant = !miniVariant"
+          <template slot="append">
+            <v-btn
+              class="mx-0"
+              depressed
+              @click="comment"
+            >
+              Post
+            </v-btn>
+          </template>
+        </v-text-field>
+      </v-timeline-item>
+
+      <v-slide-x-transition
+        group
       >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <v-container fluid>
-        <v-slide-y-transition mode="out-in">
-          <v-layout column align-center>
-            <img src="/public/v.png" alt="Vuetify.js" class="mb-5" />
-            <blockquote>
-              &#8220;First, solve the problem. Then, write the code.&#8221;
-              <footer>
-                <small>
-                  <em>&mdash;John Johnson</em>
-                </small>
-              </footer>
-            </blockquote>
+        <v-timeline-item
+          v-for="event in timeline"
+          :key="event.id"
+          class="mb-3"
+          color="pink"
+          small
+        >
+          <v-layout justify-space-between>
+            <v-flex xs7 v-text="event.text"></v-flex>
+            <v-flex xs5 text-xs-right v-text="event.time"></v-flex>
           </v-layout>
-        </v-slide-y-transition>
-      </v-container>
-    </v-content>
-    <v-navigation-drawer
-      temporary
-      fixed
-      :right="right"
-      v-model="rightDrawer"
-      app
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
-  </v-app>
+        </v-timeline-item>
+      </v-slide-x-transition>
+
+      <v-timeline-item
+        class="mb-4"
+        hide-dot
+      >
+        <span>TODAY</span>
+      </v-timeline-item>
+    </v-timeline>
+  </v-container>
+  </v-parallax>
+</v-dark>
+   
 </template>
 
 <script>
   export default {
-    data () {
-      return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'bubble_chart', title: 'Inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+    data: () => ({
+      events: [],
+      input: null,
+      nonce: 0
+    }),
+
+    computed: {
+      timeline () {
+        return this.events.slice().reverse()
+      }
+    },
+
+    methods: {
+      comment () {
+        const time = (new Date()).toTimeString()
+        this.events.push({
+          id: this.nonce++,
+          text: this.input,
+          time: time.replace(/:\d{2}\sGMT-\d{4}\s\((.*)\)/, (match, contents, offset) => {
+            return ` ${contents.split(' ').map(v => v.charAt(0)).join('')}`
+          })
+        })
+        localStorage.events = this.events
+        this.input = null
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -2,21 +2,7 @@ require('../node_modules/vuetify/src/stylus/app.styl')
 
 import Vue from 'vue'
 import {
-  Vuetify,
-  VApp,
-  VNavigationDrawer,
-  VFooter,
-  VList,
-  VBtn,
-  VIcon,
-  VGrid,
-  VToolbar,
-  transitions
-} from 'vuetify'
-import App from './App.vue'
-
-Vue.use(Vuetify, {
-  components: {
+    Vuetify,
     VApp,
     VNavigationDrawer,
     VFooter,
@@ -25,20 +11,42 @@ Vue.use(Vuetify, {
     VIcon,
     VGrid,
     VToolbar,
+    VParallax,
+    VTimeline,
+    VChip,
+    VTextField,
     transitions
-  },
-  theme: {
-    primary: '#ee44aa',
-    secondary: '#424242',
-    accent: '#82B1FF',
-    error: '#FF5252',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FFC107'
-  }
+} from 'vuetify'
+import App from './App.vue'
+
+Vue.use(Vuetify, {
+    components: {
+        VApp,
+        VNavigationDrawer,
+        VFooter,
+        VList,
+        VBtn,
+        VIcon,
+        VGrid,
+        VToolbar,
+        VParallax,
+        VTimeline,
+        VChip,
+        VTextField,
+        transitions
+    },
+    theme: {
+        primary: '#ee44aa',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+    }
 })
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })
